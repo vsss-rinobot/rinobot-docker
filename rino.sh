1cb9bbeacdfec799f12ca9773c17d2f864ad644a
Rinobot VSSS <rinobotvision@gmail.com>
2023-02-24 11:10:38 -0300

    Update

--- rino.sh
+++ rino.sh
@@ -3,4 +3,4 @@ xhost +local:docker
 
 ## Buildando o docker
 # docker build . -f Dockerfile -t nomedodocker
-docker build . -f Dockerfile -t deirazo/rinoironcup
+docker build  . -f Dockerfile -t deirazo/rinoironcup --build-arg GITHUB=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
